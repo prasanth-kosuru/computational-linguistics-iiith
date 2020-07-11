16a3c6f2733d98bf61275b75e3f7f6ddc28f6e11
prasanth-kosuru <prasanthkosuru303@gmail.com>
2020-07-11 13:23:45 +0530

    updated goal 3

---
+++ RDIP/Codes/eleven.js
@@ -0,0 +1,10 @@
+
+ function selectfun() {
+ let s = document.getElementById('ab').value;
+
+    document.getElementById('id1').innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words<br>"
+
+    document.getElementById('id2').innerHTML = "(select the buttons in proper order)"
+    }
+    
+   
